a81636a3ae2f055259921b573dc2514da9395dd9
Euan Rochester <euan@rochester.me.uk>
2019-05-05 16:53:46 +0100

    Naive soa

--- gpgpu2/impls/cuda.cu
+++ gpgpu2/impls/cuda.cu
@@ -3,7 +3,19 @@
 #include <math.h>
 #include <stdio.h>
 
-__global__ void gather(rgb* data, big_rgb* work_buffer, unsigned int width, unsigned int height, unsigned int wb_width, unsigned wb_height, unsigned int c) {
+struct rgb_soa {
+	unsigned char* r;
+	unsigned char* g;
+	unsigned char* b;
+};
+
+struct big_rgb_soa {
+	unsigned int* r;
+	unsigned int* g;
+	unsigned int* b;
+};
+
+__global__ void gather(rgb_soa data, big_rgb_soa work_buffer, unsigned int width, unsigned int height, unsigned int wb_width, unsigned wb_height, unsigned int c) {
 	unsigned int x = (blockIdx.x * 32) + threadIdx.x;
 	unsigned int y = (blockIdx.y * 32) + threadIdx.y;
 
@@ -29,20 +41,23 @@ __global__ void gather(rgb* data, big_rgb* work_buffer, unsigned int width, unsi
 
 	unsigned int cell_x = x * c;
 
-	big_rgb* output = &work_buffer[y*wb_width + x];
-
 	//averages
 	for (unsigned int cy = 0; cy < cell_height; cy++) {
 		for (unsigned int cx = 0; cx < cell_width; cx++) {
 			int i = ((cell_y + cy)*height) + cell_x + cx;
-			rgb_generic_add_assign(output, &data[i]);
+
+			work_buffer.r[y*wb_width + x] += data.r[i];
+			work_buffer.g[y*wb_width + x] += data.g[i];
+			work_buffer.b[y*wb_width + x] += data.b[i];
 		}
 	}
 
-	rgb_generic_div_assign(&work_buffer[y*wb_width + x], cell_size);
+	work_buffer.r[y*wb_width + x] /= cell_size;
+	work_buffer.g[y*wb_width + x] /= cell_size;
+	work_buffer.b[y*wb_width + x] /= cell_size;
 }
 
-__global__ void scatter(rgb* data, big_rgb* work_buffer, unsigned int width, unsigned int height, unsigned int wb_width, unsigned wb_height, unsigned int c){
+__global__ void scatter(rgb_soa data, big_rgb_soa work_buffer, unsigned int width, unsigned int height, unsigned int wb_width, unsigned wb_height, unsigned int c){
 
     unsigned int x = (blockIdx.x * 32) + threadIdx.x;
     unsigned int y = (blockIdx.y * 32) + threadIdx.y;
@@ -65,20 +80,21 @@ __global__ void scatter(rgb* data, big_rgb* work_buffer, unsigned int width, uns
     if (width%cell_width != 0 && x + 1 == wb_width) {
         cell_width = width % cell_width;
     }
-    unsigned int cell_size = cell_width * cell_height;
     unsigned int cell_x = x*c;
 
 	rgb out = {
-		(unsigned int)work_buffer[y*wb_width + x].r,
-		(unsigned int)work_buffer[y*wb_width + x].g,
-		(unsigned int)work_buffer[y*wb_width + x].b,
+		(unsigned char)work_buffer.r[y*wb_width + x],
+		(unsigned char)work_buffer.g[y*wb_width + x],
+		(unsigned char)work_buffer.b[y*wb_width + x],
 	};
 
     //copy to all ouput buffer cells
     for (unsigned int cy = 0; cy < cell_height; cy++) {
         for (unsigned int cx = 0; cx < cell_width; cx++) {
             int i = ((cell_y + cy)*height) + cell_x + cx;
-            data[i] = out;
+            data.r[i] = out.r;
+			data.g[i] = out.g;
+			data.b[i] = out.b;
         }
     }
 }
@@ -103,14 +119,31 @@ void run_cuda(big_rgb* work_buffer, rgb* data, unsigned int width, unsigned int 
 
     big_rgb global_avg = {0,0,0};
     
-    rgb* gpu_data;
-    big_rgb* gpu_wb;
+	rgb_soa gpu_data{
+		NULL,
+		NULL,
+		NULL
+	};
+    big_rgb_soa gpu_wb{
+		NULL,
+		NULL,
+		NULL
+	};
+	bail_on_cuda_error(cudaMalloc((void**)&gpu_data.r, width*height * sizeof(unsigned char)));
+	bail_on_cuda_error(cudaMalloc((void**)&gpu_data.g, width*height * sizeof(unsigned char)));
+	bail_on_cuda_error(cudaMalloc((void**)&gpu_data.b, width*height * sizeof(unsigned char)));
 
-    bail_on_cuda_error(cudaMalloc((void**)&gpu_data,width*height*sizeof(rgb)));
-    bail_on_cuda_error(cudaMalloc((void**)&gpu_wb,wb_width*wb_height*sizeof(big_rgb)));
+	bail_on_cuda_error(cudaMalloc((void**)&gpu_wb.r, wb_width*wb_height * sizeof(unsigned int)));
+	bail_on_cuda_error(cudaMalloc((void**)&gpu_wb.g, wb_width*wb_height * sizeof(unsigned int)));
+	bail_on_cuda_error(cudaMalloc((void**)&gpu_wb.b, wb_width*wb_height * sizeof(unsigned int)));
 
-    bail_on_cuda_error(cudaMemcpy(gpu_data, data, width*height*sizeof(rgb), cudaMemcpyHostToDevice));
-    bail_on_cuda_error(cudaMemset(gpu_wb, 0, wb_width*wb_height*sizeof(rgb)));
+	bail_on_cuda_error(cudaMemcpy2D(gpu_data.r, 1 * sizeof(unsigned char), &((char*)data)[0], 3 * sizeof(unsigned char), 1 * sizeof(unsigned char), width*height, cudaMemcpyHostToDevice));
+	bail_on_cuda_error(cudaMemcpy2D(gpu_data.g, 1 * sizeof(unsigned char), &((char*)data)[1], 3 * sizeof(unsigned char), 1 * sizeof(unsigned char), width*height, cudaMemcpyHostToDevice));
+	bail_on_cuda_error(cudaMemcpy2D(gpu_data.b, 1 * sizeof(unsigned char), &((char*)data)[2], 3 * sizeof(unsigned char), 1 * sizeof(unsigned char), width*height, cudaMemcpyHostToDevice));
+
+    bail_on_cuda_error(cudaMemset(gpu_wb.r, 0, wb_width*wb_height * sizeof(unsigned int)));
+	bail_on_cuda_error(cudaMemset(gpu_wb.g, 0, wb_width*wb_height * sizeof(unsigned int)));
+	bail_on_cuda_error(cudaMemset(gpu_wb.b, 0, wb_width*wb_height * sizeof(unsigned int)));
 
     //run kernel code
     dim3 blocksPerGrid((wb_width+(32 - 1))/32,(wb_height+(32 - 1))/32,1);
@@ -123,7 +156,9 @@ void run_cuda(big_rgb* work_buffer, rgb* data, unsigned int width, unsigned int 
 	cudaEventSynchronize(stop);
     bail_on_cuda_error(cudaGetLastError());
 
-    bail_on_cuda_error(cudaMemcpy(work_buffer, gpu_wb, wb_width*wb_height*sizeof(big_rgb), cudaMemcpyDeviceToHost));
+	bail_on_cuda_error(cudaMemcpy2D(&((int*)work_buffer)[0], 3 * sizeof(unsigned int), gpu_wb.r, 1 * sizeof(unsigned int), 1 * sizeof(unsigned int), wb_width*wb_height, cudaMemcpyDeviceToHost));
+	bail_on_cuda_error(cudaMemcpy2D(&((int*)work_buffer)[1], 3 * sizeof(unsigned int), gpu_wb.g, 1 * sizeof(unsigned int), 1 * sizeof(unsigned int), wb_width*wb_height, cudaMemcpyDeviceToHost));
+	bail_on_cuda_error(cudaMemcpy2D(&((int*)work_buffer)[2], 3 * sizeof(unsigned int), gpu_wb.b, 1 * sizeof(unsigned int), 1 * sizeof(unsigned int), wb_width*wb_height, cudaMemcpyDeviceToHost));
 
     //TODO: use a cuda reduction to speed this up & avoid copying wb to cpu
     for(int i=0;i<wb_width*wb_height;i++){
@@ -134,10 +169,16 @@ void run_cuda(big_rgb* work_buffer, rgb* data, unsigned int width, unsigned int 
 	//divide by count for global average
     big_rgb_div_assign(&global_avg, width*height);
 
-    bail_on_cuda_error(cudaMemcpy(data, gpu_data, width*height*sizeof(rgb), cudaMemcpyDeviceToHost));
+	bail_on_cuda_error(cudaMemcpy2D(&((char*)data)[0], 3 * sizeof(unsigned char), gpu_data.r, 1 * sizeof(unsigned char), 1 * sizeof(unsigned char), width*height, cudaMemcpyDeviceToHost));
+	bail_on_cuda_error(cudaMemcpy2D(&((char*)data)[1], 3 * sizeof(unsigned char), gpu_data.g, 1 * sizeof(unsigned char), 1 * sizeof(unsigned char), width*height, cudaMemcpyDeviceToHost));
+	bail_on_cuda_error(cudaMemcpy2D(&((char*)data)[2], 3 * sizeof(unsigned char), gpu_data.b, 1 * sizeof(unsigned char), 1 * sizeof(unsigned char), width*height, cudaMemcpyDeviceToHost));
     
-    bail_on_cuda_error(cudaFree(gpu_wb));
-    bail_on_cuda_error(cudaFree(gpu_data));
+    bail_on_cuda_error(cudaFree(gpu_wb.r));
+	bail_on_cuda_error(cudaFree(gpu_wb.g));
+	bail_on_cuda_error(cudaFree(gpu_wb.b));
+    bail_on_cuda_error(cudaFree(gpu_data.r));
+	bail_on_cuda_error(cudaFree(gpu_data.g));
+	bail_on_cuda_error(cudaFree(gpu_data.b));
 
 	// Output the average colour value for the image
 	printf("CUDA Average image colour red = %u, green = %u, blue = %u \n",(unsigned char)global_avg.r,(unsigned char)global_avg.g,(unsigned char)global_avg.b);
